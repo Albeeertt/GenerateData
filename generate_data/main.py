bb3aa7645f83f2cec8b33710c5f6a8cdbc409d9f
Albeeertt <alberto.sanchezsoto16@gmail.com>
2025-05-29 10:52:23 +0200

    first version

--- generate_data/main.py
+++ generate_data/main.py
@@ -13,6 +13,7 @@ from functools import partial
 from importlib import resources
 import numpy as np
 import logging
+from sklearn.model_selection import train_test_split
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,6 +29,27 @@ from mi_herramienta.utils.wrapper import Wrapper
 from mi_herramienta.utils.split import split_into_chunks, split_list_into_tables
 from mi_herramienta.utils.agat import Agat
 
+
+def save_chunks_to_json(X_chunk, y_chunk, filename):
+    """Guarda un ndarray en JSON de forma eficiente usando base64."""
+    def toBase64(array):
+
+        array_bytes = array.tobytes()  # Convierte a binario
+        array_b64 = base64.b64encode(array_bytes).decode("utf-8")  # Codifica en base64
+        
+        return {
+            "shape": array.shape,
+            "dtype": str(array.dtype),
+            "data": array_b64
+        }
+
+    mode = 'w' if not os.path.exists(filename) else 'a'
+
+    with open(filename, mode) as file:
+        for x_table, y_table in zip(X_chunk, y_chunk):
+            json.dump({"X": toBase64(x_table), "Y": toBase64(y_table)}, file)
+            file.write("\n")
+
 def obtener_argumentos():
     parser = argparse.ArgumentParser()
 
@@ -35,7 +57,7 @@ def obtener_argumentos():
     parser.add_argument('--fasta', type=str, required=True, help="Ruta hasta el archivo fasta.")
     # parser.add_argument('--k', type=int, required=False, help="Tamaño del kmer.")
     parser.add_argument('--repeatMask', type=str, required=False, help="Mask of transposable elements in gff3.")
-    parser.add_argument('--add_labels', type=bool, required=False, help="Add introns, intergenic regions and keep the longest isoform")
+    parser.add_argument('--add_labels', type="store_true", required=False, help="Add introns, intergenic regions and keep the longest isoform")
     parser.add_argument('--n_cpus', type=int, required=True, help="Número de cpus a usar")
     parser.add_argument('--out', type=str, required=True, help="Carpeta donde se va a alojar row_w_high_KLDivergence")
     
@@ -46,6 +68,7 @@ def obtener_argumentos():
 
 def ejecutar():
     args = obtener_argumentos()
+    random_state = 12
 
     if args.add_labels:
         route_out: str = ("/".join(args.gff.split("/")[:-1]))+"/"
@@ -56,12 +79,6 @@ def ejecutar():
 
 
 
-
-    default_model: str = "end_800.keras"
-    model_path = (
-        resources.files("mi_herramienta")  
-                 .joinpath("models", default_model)
-    )
     default_value_k: int = 7
     default_encoding: str = "latin-1"
     seleccionados : List[str] = ['exon', 'intron', 'transposable_element', 'intergenic_region']
@@ -115,91 +132,64 @@ def ejecutar():
 
     new_list = []
     for list_cpu in results_clean_data: new_list.extend(list_cpu)
+
+
+    # -------------------
+    # Aquí entran los cambios.
+    # -------------------
     
-    LIMITE = DEFAULT_LIMITE if len(new_list) >= DEFAULT_LIMITE else len(new_list)
+    LIMITES = [("20_porciento", .2*len(new_list)), ("45_porciento", .45*len(new_list)), ("70_porciento", .7*len(new_list)), ("90_porciento", .9*len(new_list))]
+    filename_incomplete: str = "dataset_"
+    extension_filename: str = ".json"
+    train: str = "train_"
+    validation: str = "validation_"
+    test: str = "test_"
 
-
-    tables_fixed = partial(create_tables_instance.complete_table, limite = LIMITE, solapamiento = True)
-
-    tables_adapter = wrap_functions_instance.make_adapter(
-        tables_fixed,
-        input_selector= wrap_functions_instance.tuple_chunk,
-        output_selector= wrap_functions_instance.output_res
-    )
-
-    scheduler_cleanData = Scheduler([tables_adapter], split_list_into_tables)
-    result_data_x = scheduler_cleanData.run(new_list, LIMITE, n_cpu) # lista de tuplas
-    results_tables_x = []
-    for result, _ in result_data_x: results_tables_x.extend(result)
-
-    result_data_y = np.zeros_like(results_tables_x[0])
-    for tables_x, restante in result_data_x:
-        if restante is not None:
-            np.add(result_data_y, restante, out=result_data_y)
-        for table_x in tables_x:
-            np.add(result_data_y, table_x, out=result_data_y)
-
-    result_data_y = create_tables_instance.normalize_rows(result_data_y)
-    results_tables_x = [create_tables_instance.normalize_rows(table) for table in results_tables_x]
-    results_tables_y = [result_data_y for _ in range(len(results_tables_x))]
     
+    # LIMITE = DEFAULT_LIMITE if len(new_list) >= DEFAULT_LIMITE else len(new_list)
+    for idx, (identifier, LIMITE) in enumerate(LIMITES):
 
-    # --------------------------------------------------------------------------------------------
+
+        tables_fixed = partial(create_tables_instance.complete_table, limite = LIMITE, solapamiento = True)
+
+        tables_adapter = wrap_functions_instance.make_adapter(
+            tables_fixed,
+            input_selector= wrap_functions_instance.tuple_chunk,
+            output_selector= wrap_functions_instance.output_res
+        )
+
+        scheduler_cleanData = Scheduler([tables_adapter], split_list_into_tables)
+        result_data_x = scheduler_cleanData.run(new_list, LIMITE, n_cpu) # lista de tuplas
+        results_tables_x = []
+        for result, _ in result_data_x: results_tables_x.extend(result)
+        results_tables_x = [create_tables_instance.normalize_rows(table) for table in results_tables_x]
+
+        if idx == 0:
+            result_data_y = np.zeros_like(results_tables_x[0])
+            for tables_x, restante in result_data_x:
+                if restante is not None:
+                    np.add(result_data_y, restante, out=result_data_y)
+                for table_x in tables_x:
+                    np.add(result_data_y, table_x, out=result_data_y)
+
+            result_data_y = create_tables_instance.normalize_rows(result_data_y)
+            results_tables_y = [result_data_y for _ in range(len(results_tables_x))]
+
+            y_data = np.array(results_tables_y)
+
+        
+        X_data = np.array(results_tables_x)
+        X_data = np.expand_dims(X_data, axis=-1)
+
+        X_train, X_tmp, y_train, y_tmp = train_test_split(X_data, y_data, test_size=.4, random_state = random_state)
+        X_validation, X_test, y_validation, y_test = train_test_split(X_tmp, y_tmp, test_size=.2, random_state = random_state)
+
+        # Dividir entre train, validantion y test
+        save_chunks_to_json(X_train, y_train, filename_incomplete+train+identifier+extension_filename)
+        save_chunks_to_json(X_validation, y_validation, filename_incomplete+validation+identifier+extension_filename)
+        save_chunks_to_json(X_test, y_test, filename_incomplete+test+identifier+extension_filename)
 
 
-    X_data = np.array(results_tables_x)
-    X_data = np.expand_dims(X_data, axis=-1)
 
-    y_data = np.array(results_tables_y)
 
-    # Seleccionar el modelo adecuado.
     
-    model = tf.keras.models.load_model(model_path)
-    resultado = model.predict(X_data, batch_size = 1)
-
-    mse = tf.keras.losses.MeanSquaredError()(y_data, resultado).numpy()
-    kl_div = tf.keras.losses.KLDivergence()(y_data, resultado).numpy()
-    acc = tf.keras.metrics.categorical_accuracy(y_data, resultado)
-    cat_acc = tf.reduce_mean(acc).numpy()
-
-    print("MSE:", mse)
-    print("KL Divergence:", kl_div)
-    print("Accuracy: ",round(cat_acc*100,2), "%")
-    
-    
-# ------------------------
-
-    best_klDivergence: int = 10
-    best_acc: int = 0
-    tabla_elegida: int = 0
-
-    # Obtener el mejor resultado:
-    for idx, (y_true, y_pred) in enumerate(zip(y_data, resultado)):
-
-        kl_div = tf.keras.losses.KLDivergence()(y_true, y_pred).numpy()
-        acc = tf.keras.metrics.categorical_accuracy(y_true, y_pred)
-        cat_acc = tf.reduce_mean(acc).numpy()
-
-        if cat_acc > best_acc:
-            best_acc = cat_acc
-        if kl_div < best_klDivergence:
-            tabla_elegida = idx
-            best_klDivergence = kl_div
-
-    print("Mejor KLDivergence: ", best_klDivergence)
-    print("Mejor Accuracy: ", round(best_acc,2), "%")
-
-
-# ------------------------
-
-    umbral = 0.25
-    filas_altas = []
-
-    with open(args.out+"/row_w_high_KLDivergence.txt", "a") as f:
-        for i in range(y_data[tabla_elegida].shape[0]):
-            kl_val = tf.keras.losses.KLDivergence()(y_data[tabla_elegida][i], resultado[tabla_elegida][i]).numpy()
-            
-            if kl_val > umbral:
-                f.write(f"Kmer: {create_tables_instance.kmer_mapping_inverse[i]} - KL: {kl_val:.4f}\n")
-                f.write("       Valor real: " + " ".join([f" {seleccionados[idx]} - {round(v*100,2):.4f}% |" for idx, v in enumerate(y_data[tabla_elegida][i])]) + "\n")
-                f.write("       Valor predicho: " + " ".join([f" {seleccionados[idx]} - {round(v*100,2):.4f}% |" for idx, v in enumerate(resultado[tabla_elegida][i])]) + "\n")
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="mi_herramienta",
+    name="generate_data",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
       package_data={                    
-        "mi_herramienta": ["models/*.keras", "models/*.pt"],
+        "generate_data": ["models/*.keras", "models/*.pt"],
     },
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'KMAsgec=mi_herramienta.cli:main',
+            'KMAsgec=generate_data.cli:main',
         ],
     },
 )
